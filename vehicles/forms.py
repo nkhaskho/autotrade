0c4129a1dddcf6be677bf368cb9d8200fbd06206
Naiim Khaskhoussi <khaskhoussi.naiim@gmail.com>
2021-01-11 17:00:02 +0100

    Adding AddVehicleForm

--- vehicles/forms.py
+++ vehicles/forms.py
@@ -1,2 +1,35 @@
 from django import forms
 from vehicles.models import *
+
+class AddVehicleForm(forms.Form):
+    #for_sale, model, brand, fuel_type, kilometers, price, owner
+    for_sale = forms.BooleanField(
+        label="For sale: ", 
+        initial=True,
+        widget=forms.Select(
+            attrs={
+                "class": "form-control"
+            },
+            choices=[("For sale", "Not for sale")]
+        )
+    )
+    model = forms.CharField(
+        label="Model: ", 
+        initial=None,
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control"
+            }
+        )
+    )
+
+    brand = forms.Select(
+        label="Password: ", 
+        widget=forms.Select(
+            attrs={
+                "class": "form-control"
+            },
+            choices=Brand.objects.all()
+        )
+    )
+
